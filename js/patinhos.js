5d673a0e511f91b67f2f000a0fddd1662a6c69e5
viniiciusaantos <viniiciusaantos>
2019-08-30 16:39:08 -0300

    First commit

---
+++ js/patinhos.js
@@ -0,0 +1,15 @@
+for (var patinhos = 5; patinhos > 2; patinhos--){
+    console.log(patinhos + ' patinhos foram passear além das montanhas para brincar')
+    console.log('A mamãe gritou quá quá quá quá')
+    console.log('Mas só ' + (patinhos-1) + ' patinhos voltaram de lá')
+}
+
+for (var patinhos; patinhos > 1; patinhos--){
+    console.log(patinhos + ' patinhos foram passear além das montanhas para brincar')
+    console.log('A mamãe gritou quá quá quá quá')
+    console.log('Mas só ' + (patinhos-1) + ' patinho voltou de lá')
+}
+
+console.log (patinhos + ' patinho foi passear além das montanhas para brincar')
+console.log('A mamãe gritou quá quá quá quá')
+console.log('Mas nenhum patinho voltou de lá')
